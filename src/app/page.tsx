8d18ab5719d54397d0f7e8f0c65ded6f74d8da2f
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-11-22 14:34:34 +0600

    home page setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Navber from "@/components/shared/Navber";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <Navber />
+      <div className="flex justify-between max-w-7xl mx-auto gap-4 ">
+        <div className="border-2 border-amber-500 w-full flex-1">Left</div>
+        <div className="border-2 border-amber-500 w-full flex-2">Middle</div>
+        <div className="border-2 border-amber-500 w-full flex-1">Right</div>
+      </div>
     </div>
   );
 }
