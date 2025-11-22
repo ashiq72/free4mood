f064a601afaf969bcd3423dfeeda8427eaa0efee
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-11-23 05:55:11 +0600

    file stuctrue ready

--- src/app/(withCommonLayout)/(Feed)/page.tsx
+++ src/app/(withCommonLayout)/(Feed)/page.tsx
@@ -1,9 +1,6 @@
-import Navber from "@/components/shared/Navber";
-
 export default function Home() {
   return (
     <div>
-      <Navber />
       <div className="flex justify-between max-w-7xl mx-auto gap-4 ">
         <div className="border-2 border-amber-500 w-full flex-1">Left</div>
         <div className="border-2 border-amber-500 w-full flex-2">Middle</div>
